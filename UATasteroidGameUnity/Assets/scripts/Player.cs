81247263ca241250abda61673d85495a344b564d
Shane <sdoring1987@gmail.com>
2020-06-18 21:22:06 -0700

    Tank controls implemented and rigid body added

--- UATasteroidGameUnity/Assets/scripts/Player.cs
+++ UATasteroidGameUnity/Assets/scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Transform tf;
+    public float turnSpeed = 1f;
+    public float moveSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,25 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+         tf.Rotate(0, 0, turnSpeed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            tf.Rotate(0, 0, -turnSpeed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+    public void Shoot()
     {
         
     }
