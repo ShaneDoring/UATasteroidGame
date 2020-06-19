ef48cd8d8247ba305ccf351a1ded1540862a7192
Shane <sdoring1987@gmail.com>
2020-06-18 20:32:17 -0700

    added GameManager Object

---
+++ UATasteroidGameUnity/Assets/scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject player;
+
+    public int score;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.LogError("[Game manager attempted to make a second game manager: ]" + this.gameObject.name);
+        }
+    }
+}
+  
